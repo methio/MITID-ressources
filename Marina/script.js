2e1b4ed0eb2a7ee1c4eec1f220c5de2dc5d31b46
Thomas Perivolas <thomas.perivolas@gmail.com>
2025-10-27 14:16:26 +0100

    added rate + example for monosynth melody

--- Marina/script.js
+++ Marina/script.js
@@ -13,11 +13,13 @@ const reverbSlider = document.getElementById('reverb');
 let reverb; 
 const volumeSlider = document.getElementById('volume');
 
+const rateSlider = document.getElementById('rate');
+let ratee;
+
+// Sine wave oscillator
+let sineOsc; 
 const sineAmp = document.getElementById('sineAmp');
 const sineFreq = document.getElementById('sineFreq');
-
-
-let sineOsc; 
 
 let bg;
 let volume = .5;
@@ -32,7 +34,6 @@ function setup() {
     createCanvas(400, 400);
     reverb = new p5.Reverb();
     sineOsc = new p5.Oscillator('sine');
-    
 }
 
 function draw() {
@@ -84,7 +85,119 @@ reverbSlider.addEventListener('input', (event) => {
     // song.amp(amplitude);
 });
 
+rateSlider.addEventListener('input', (event) => {
+    ratee = parseFloat(event.target.value);
+    console.log(ratee, typeof(ratee));
+    song.rate(ratee);
+});
+
 sineAmp.addEventListener("input", (event) => {
     const amp = parseFloat(event.target.value);
-    
+    sineOsc.amp(amp);
 });
+
+sineFreq.addEventListener("input", (event) => {
+    const freq = parseFloat(event.target.value);
+    sineOsc.freq(freq);
+});
+
+const buttonNote = document.getElementById('button-note');
+buttonNote.addEventListener('click', () => {
+    playSound(monoSynth, 440, 500);
+});
+
+const playSound = (module, frequency, duration) => {
+    let note = random(['Fb4', 'G4']);
+    // note velocity (volume, from 0 to 1)
+    let velocity = random();
+    // time from now (in seconds)
+    let time = 0;
+    // note duration (in seconds)
+    let dur = 1/6;
+
+    module.play(note, velocity, time, dur);
+};
+
+
+// // Variable for frequency (middle C).
+// let myFreq = 262;
+
+// // Array of frequencies in C Major.
+// let frequencies = [
+//   myFreq,
+//   myFreq * 9/8,
+//   myFreq * 5/4,
+//   myFreq * 4/3,
+//   myFreq * 3/2,
+//   myFreq * 5/3,
+//   myFreq * 15/8,
+//   myFreq * 2
+// ];
+
+// // Melody Object for C Major
+// let melody = {
+//   name: 'C Major Scale',
+//  notesIndex: [0, 1, 2, 3, 4, 5, 6, 7],
+//  tempo: 120
+// };
+
+// // Empty array for oscillator objects.
+// let oscillators = [];
+
+// // Calculate duration of each note in seconds.
+// let noteDuration = 60 / melody.tempo;
+
+// function setup() {
+//   createCanvas(400, 400);
+
+//   // Initialize oscillators and place in oscillators array.
+//   for (let freq of frequencies) {
+//     osc = new p5.Oscillator(freq);
+//     oscillators.push(osc);
+//   }
+
+//   //set color mode to HSB (better for using notes to color keys)
+//   colorMode(HSB);
+// }
+
+// function draw() {
+//   background(220);
+//   drawMelody();  
+// }
+
+// // Starts playing the note.
+// function playNote(n) {
+//   // Starts oscillator if needed.
+//   if (oscillators[n].started === false) {
+//     oscillators[n].start();
+
+//     // Starts playing the note by increasing the volume with a 0.01 sec fade-in.
+//     oscillators[n].amp(1, 0.01);
+//   }
+
+//   // Stops playing the note after number of seconds stored in noteDuration * 1000
+//   setTimeout(stopNote, noteDuration * 1000, n); 
+// }
+
+// // Stops playing the note.
+// function stopNote(n) {
+//   // Lower oscillator volume to 0.
+//   oscillators[n].amp(0, 0.01);
+
+//   // Stop the oscillator.
+//   oscillators[n].stop();
+// }
+
+// // Plays the notes in a melody.
+// function play() {
+//   // Read each [index, note] in melody.notesIndex
+//   for (let [index, note] of melody.notesIndex.entries()) {
+//     // Play each note at scheduled time
+//     setTimeout(playNote, noteDuration * 1000 * index, note);
+//   }
+// }
+
+// //play melody with mouse click
+// function mousePressed() {
+//   play();
+// }
